4b0d9a0502f7f6c8a147d59c5b755ca7fdeed171
Roger Mayer <rmayer1984@gmail.com>
2019-11-15 16:18:00 -0600

    finished ex

--- roger-js.js
+++ roger-js.js
@@ -1,8 +1,8 @@
 "use strict";
-$('#submit').click(function(e) {
+$('#submit').click(function (e) {
     e.preventDefault();
     let val = $('input').val().toLowerCase();
-    val = val.replace(/\s/g,'');
+    val = val.replace(/\s/g, '');
     console.log(val);
     if (typeof val === 'string') {
         $('body').css('background-color', `${val}`);
@@ -11,41 +11,41 @@ $('#submit').click(function(e) {
     } //need to figure out how to return something if color is not available
 });
 
-// $('#submitTwo').click(function(e) {
-//     e.preventDefault();
-//     let z = $('.pushTwo').val();
-//     z = Number(z);
-//     console.log(z);
-//     if (typeof z === 'number' && z > 0 && z <=10) {
-//             let x,y,chr;
-//             for(x=1; x <=z + 1; x++)
-//             {
-//                 for (y=1; y < x; y++)
-//                 {
-//                     chr= chr+("*");
-//                 }
-//                 $('h2').html(chr);
-//                 chr='';
-//             }
-//
-//     } else {
-//         $('h2').html('That is not a number between one and ten');
-//     }
-// });
-
-var z = parseInt(prompt("how long would you like your triangle?"));
-var x,y,chr;
-function pyramid(z) {
-
-    for(x=1; x <= z + 1 ; x++)
-    {
-        for (y=1; y < x; y++)
-        {
-            chr=chr+("*");
+$('#submitTwo').click(function (e) {
+    e.preventDefault();
+    let z = $('.pushTwo').val();
+    z = Number(z);
+    console.log(z);
+    $('h2').html("");
+    if (typeof z === 'number' && z > 0 && z <= 10) {
+        let x, y;
+        let chr = "";
+        for (x = 1; x <= z + 1; x++) {
+            for (y = 1; y < x; y++) {
+                chr = chr + ("*");
+            }
+            $('h2').append(`${chr} <br>`);
+            chr = '';
         }
-        console.log(chr);
-        chr='';
-    }
-}
 
-console.log(pyramid(z));
+    } else {
+        $('h2').html('That is not a number between one and ten');
+    }
+});
+
+// var z = parseInt(prompt("how long would you like your triangle?"));
+// var x,y,chr;
+// function pyramid(z) {
+//
+//     for(x=1; x <= z + 1 ; x++)
+//     {
+//         for (y=1; y < x; y++)
+//         {
+//             chr=chr+("*");
+//         }
+//         console.log(chr);
+//         chr='';
+//     }
+// }
+//
+// console.log(pyramid(z));
