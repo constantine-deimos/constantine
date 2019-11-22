58d9cea55414a3415013d4b1da4969c121cf0cac
Roger Mayer <rmayer1984@gmail.com>
2019-11-22 13:36:17 -0600

    completed 2 js exercises

--- roger-js.js
+++ roger-js.js
@@ -33,3 +33,43 @@ $('#submitTwo').click(function (e) {
     }
 });
 
+//Given an array, find the int that appears an odd number of times.
+function findOdd(numbers) {
+    var count = 0;
+    for(var i = 0; i<numbers.length; i++){
+        for(var j = 0; j<numbers.length; j++){
+            if(numbers[i] == numbers[j]){
+                count++;
+            }
+        }
+        if(count % 2 != 0 ){
+            return numbers[i];
+        }
+    }
+};
+
+console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])); //5
+console.log(findOdd([1,1,1,1,1,1,10,1,1,1,1])); //10
+console.log(findOdd([1,1,1,1,1,1,10,1,1,1,1])); //10
+//return array with smallest number removed
+
+function removeSmallest(numbers) {
+    var outputArr = []; //created bucket
+    var smallest = numbers[0];
+    for (var i = 0; i < numbers.length; i++) {
+        if (numbers[i] < smallest) {
+            smallest = numbers[i]; //get index of smallest number
+        }
+    }
+    for (var j = 0; j < numbers.length; j++) {
+        if (j !== numbers.indexOf(smallest)) {
+            outputArr.push(numbers[j]); //create array not including index of smallest number
+        }
+    }
+
+    return outputArr;
+}
+
+console.log(removeSmallest([5, 3, 2, 1, 4]));
+
+
